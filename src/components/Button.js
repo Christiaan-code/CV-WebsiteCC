be3e3b81f63e070ce673f84dcf7c2a8858eebd1f
Christiaan <30540070@student.g.nwu.ac.za>
2020-10-08 17:30:49 +0200

    Finished nav bar and started Hero

--- src/components/Button.js
+++ src/components/Button.js
@@ -13,10 +13,10 @@ export const Button = ({children, type, onClick, buttonStyle,
 
         const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
         return (
-            <Link to='/' className ='btn-mobile'>
+            <Link to='/sign-up' className ='btn-mobile'>
                 <button
                 className={`btn ${checkButtonStyle} ${checkButtonSize}`}
-                type={onClick}
+                type= {onClick}
                 type= {type}
                 >
                     {children}
